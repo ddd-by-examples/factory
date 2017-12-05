58e6c31798d87e3f47c89f87a7b449c0bd66cefa
Michał Michaluk <michaluk.michal@gmail.com>
2017-12-05 09:46:14 +0100

    delivery auto planning draft

--- app-monolith/src/main/java/pl/com/bottega/factory/delivery/planning/DeliveryAutoPlannerRepository.java
+++ app-monolith/src/main/java/pl/com/bottega/factory/delivery/planning/DeliveryAutoPlannerRepository.java
@@ -1,7 +1,10 @@
 package pl.com.bottega.factory.delivery.planning;
 
+import org.springframework.stereotype.Component;
+import pl.com.bottega.factory.delivery.planning.DeliveryAutoPlanner;
 import pl.com.bottega.factory.product.management.RefNoId;
 
+@Component
 public class DeliveryAutoPlannerRepository {
     public DeliveryAutoPlanner get(RefNoId refNo) {
         return null;
