ac6b4e9519c988ef30edb9739b9cc2afd41bc7c7
Jakub Pilimon <jakub.pilimon@gmail.com>
2018-03-11 13:00:59 +0100

    possibility to run the app with in memory h2 database for integration testing

--- app-monolith/src/main/resources/schema/product-management.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset michaluk.michal:1.init
-CREATE SCHEMA product_management;
-
-CREATE TABLE product_management.product_description (
-    ref_no character varying(64) NOT NULL PRIMARY KEY,
-    description json NOT NULL
-);
